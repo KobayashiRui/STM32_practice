fcf80f44ce270dd5feb035dd49ddf3be1caf55ca
KobayashiRui <roboroborx782@gmail.com>
2019-09-02 21:56:19 +0900

    update

--- stm32_f303k8_can_odrive_test/Src/main.c
+++ stm32_f303k8_can_odrive_test/Src/main.c
@@ -52,7 +52,12 @@ CAN_TxHeaderTypeDef TxHeader;
 CAN_RxHeaderTypeDef RxHeader;
 uint8_t TxData[8];
 uint8_t RxData[8];
+int set_point=10000;
 uint32_t TxMailbox;
+uint8_t motor_num = 1; //odrive axis id
+//uint8_t control_mode = 0x00C;//control mode
+//uint8_t control_mode = 0x009;
+uint8_t control_mode = 0x017;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -67,7 +72,9 @@ static void MX_CAN_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 uint8_t uart_data;
-char tx_data[]="get_data\r\n";
+char tx_data[]="getdata\r\n";
+char tx_data1[]="encoder\r\n";
+char tx_data2[]="vel\r\n";
 int flag = 1;
 /* USER CODE END 0 */
 
@@ -103,6 +110,7 @@ int main(void)
   MX_USART2_UART_Init();
   MX_CAN_Init();
   /* USER CODE BEGIN 2 */
+
   sFilterConfig.FilterBank = 0;
   sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
   sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
@@ -113,6 +121,7 @@ int main(void)
   sFilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0;
   sFilterConfig.FilterActivation=ENABLE;
   sFilterConfig.SlaveStartFilterBank=14;
+
   if(HAL_CAN_ConfigFilter(&hcan,&sFilterConfig) != HAL_OK)
   {
     Error_Handler();
@@ -126,14 +135,31 @@ int main(void)
   {
 	Error_Handler();
   }
-  TxHeader.StdId=0x323;
-  TxHeader.ExtId=0x01;
-  TxHeader.RTR = CAN_RTR_DATA;
+  TxHeader.StdId=(motor_num << 5) + (control_mode);
+  //TxHeader.ExtId=0;
+  TxHeader.RTR = 2;//CAN_RTR_DATA;
   TxHeader.IDE = CAN_ID_STD;
-  TxHeader.DLC = 2;
+  TxHeader.DLC = 0x08;
   TxHeader.TransmitGlobalTime = DISABLE;
-  TxData[0] = 'A';
-  TxData[1] = 'B';
+  /*
+  TxData[0] = set_point;
+  TxData[1] = set_point >> 8;
+  TxData[2] = set_point >> 16;
+  TxData[3] = set_point >> 24;
+  TxData[4] = 0;
+  TxData[5] = 0;
+  TxData[6] = 0;
+  TxData[7] = 0;
+  */
+  TxData[0] = 0;
+  TxData[1] = 0;
+  TxData[2] = 0;
+  TxData[3] = 0;
+  TxData[4] = 0;
+  TxData[5] = 0;
+  TxData[6] = 0;
+  TxData[7] = 0;
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -144,7 +170,20 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 	HAL_CAN_AddTxMessage(&hcan,&TxHeader,TxData,&TxMailbox);
-	HAL_Delay(100);
+	HAL_Delay(1000);
+	  for(int j=0; j< 4; j++){
+		  HAL_UART_Transmit(&huart2,&RxData[j],1,0xFFFF);
+		  HAL_Delay(10);
+	  }
+	  HAL_UART_Transmit(&huart2,tx_data,sizeof(tx_data),0xFFFF);
+	  HAL_Delay(10);
+	//HAL_UART_Transmit(&huart2,RxData,8,0xFFFF);
+	  for(int j=4; j< 8; j++){
+		  HAL_UART_Transmit(&huart2,&RxData[j],1,0xFFFF);
+		  HAL_Delay(10);
+	  }
+	  HAL_UART_Transmit(&huart2,tx_data,sizeof(tx_data),0xFFFF);
+	  HAL_Delay(10);
   }
   /* USER CODE END 3 */
 }
@@ -163,7 +202,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -172,12 +213,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -199,7 +240,7 @@ static void MX_CAN_Init(void)
 
   /* USER CODE END CAN_Init 1 */
   hcan.Instance = CAN;
-  hcan.Init.Prescaler = 16;
+  hcan.Init.Prescaler = 6;
   hcan.Init.Mode = CAN_MODE_NORMAL;
   hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan.Init.TimeSeg1 = CAN_BS1_10TQ;
@@ -236,7 +277,7 @@ static void MX_USART2_UART_Init(void)
 
   /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
+  huart2.Init.BaudRate = 9600;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
@@ -290,7 +331,18 @@ void HAL_CAN_TxMailbox0CompleteCallack(CAN_HandleTypeDef *hcan)
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan_)
 {
   HAL_CAN_GetRxMessage(&hcan,CAN_RX_FIFO0,&RxHeader,RxData);
-  HAL_UART_Transmit(&huart2,RxData,sizeof(RxData),0xFFFF);
+
+  //HAL_UART_Transmit(&huart2,&RxHeader.StdId,sizeof(&RxHeader.StdId),0xFFFF);
+
+  /*
+  for(int j=0; j< 4; j++){
+	  HAL_UART_Transmit(&huart2,&RxData[j],1,0xFFFF);
+	  HAL_Delay(10);
+  }
+*/
+  //HAL_UART_Transmit(&huart2,RxData,4,0xFFFF);
+
+
   flag *= -1;
   if(flag >0){
 	  HAL_GPIO_WritePin(GPIOB,GPIO_PIN_3,1);
@@ -299,6 +351,8 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan_)
   }
 
 }
+
+
 /* USER CODE END 4 */
 
 /**
